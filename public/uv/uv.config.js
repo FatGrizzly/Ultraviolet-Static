ba7761e025d6d2112cae0fdd371e71e99db3a29b
FatGrizzly <66728880+FatGrizzly@users.noreply.github.com>
2023-06-29 22:12:58 +0530

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -2,7 +2,7 @@
 
 self.__uv$config = {
   prefix: "/uv/service/",
-  bare: "/bare/",
+  bare: "https://use.astroid.wtf/bare/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
